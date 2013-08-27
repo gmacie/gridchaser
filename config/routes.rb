52528f4464d33b9340ec82aebe258400186a0a93
Gordon Macie <gmacie@gmail.com>
2013-08-26 22:28:37 -0400

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Gridchaser::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'pages/home'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
